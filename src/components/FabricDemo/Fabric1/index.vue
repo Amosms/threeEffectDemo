570d0d605f28b2fbeedc1a117c62e1ba8cdcd4fd
shuai.ma@usmartww.com <shuai.ma@usmartww.com>
2024-07-04 13:59:20 +0800

    添加fabric模块

---
+++ src/components/FabricDemo/Fabric1/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div style="padding: 20px">
+    <canvas
+        width="500"
+        height="500"
+        id="canvas"
+        style="border: 1px solid rgba(69, 154, 235, 0.55)"
+    ></canvas>
+  </div>
+</template>
+<script lang="ts">
+console.log("222");
+
+</script>
+<style>
+canvas {
+    border: 1px solid red;
+}
+</style>
---
+++ src/components/FabricDemo/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="fabricDemo">
+    <router-view></router-view>
+  </div>
+</template>
+<style>
+.fabricDemo {
+  width: 100%;
+  height: 100%;
+}
+</style>
